c8b9781aa6c795fdfb7250545b260ddb20c680c7
José Carlos Anaya Chávez <josecarlosanach@MacBook-Pro-de-Jose.local>
2018-10-30 07:26:09 -0600

    email sender and status change on the ToDos HoundCI first fixes

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -32,7 +32,7 @@ class TodosController < ApplicationController
   def create
     @todo = Todo.new(todo_params)
     @todo.user_id = current_user.id
-    #binding.pry
+    # binding.pry
 
     respond_to do |format|
       if @todo.save
--- config/application.rb
+++ config/application.rb
@@ -25,9 +25,8 @@ module Apptodo
     config.before_configuration do
       env_file = File.join(Rails.root, 'config', 'local_env.yml')
       YAML.load(File.open(env_file)).each do |key, value|
-        ENV[key.to_s] = value
+      ENV[key.to_s] = value
       end if File.exist?(env_file)
     end
-
   end
 end
